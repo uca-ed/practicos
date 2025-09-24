ecbfb7a232b27f93a7f30b56351a5a9d9c3cd0d2
AgustinSafronchik <agussafronchik@gmail.com>
2025-09-24 20:11:03 -0300

    .

--- 01.Grafos/archivos_ej3/ej3.py
+++ 01.Grafos/archivos_ej3/ej3.py
@@ -3,23 +3,33 @@ from collections import deque
 
 def cargarGrafo(ruta_json):
     f=open(ruta_json, "r")
-    estructura = json.load(f)
+    estructura=json.load(f)
     return estructura["E"]
 
-def buscarPaso(E, s, t):
-    openLst = deque([(s, None)])
-    closedLst = []
-    padre = {s: None}
+def buscarPaso(E, s, t): #Busqueda de Paso
+    openLst=deque([(s, None)])
+    closedLst=[]
+    padre={s: None}
     while openLst!=None:
         z, y = openLst.popleft()
         closedLst.append((z, y))
-        if t in E.get(z, []):
-            padre[t] = z
+        
+        if z in E:
+            camino=E[z]
+        else:
+            camino=[]
+        if t in camino:
+            padre[t]=z
             return reconstruirCamino(padre, t)
-        enOpenLst = {n for (n, _) in openLst}
-        enClosedLst = {n for (n, _) in closedLst}
+        
+        for nodoPrimero in openLst:
+            n=nodoPrimero[0]
+            openLst.add(n)
+        for nodoPrimero in closedLst:
+            n=nodoPrimero[0]
+            closedLst.add(n)
         for w in E.get(z, []):
-            if w not in enOpenLst and w not in enClosedLst and w not in padre:
+            if w not in openLst and w not in closedLst and w not in padre:
                 padre[w] = z
                 openLst.append((w, z))
     return None
@@ -32,12 +42,12 @@ def reconstruirCamino(padre,t):
     return camino
 
 def main():
-    grafo = cargarGrafo("esDivisorDe-200.json")
-    s, t = "1", "192" #Sea s el nodo inicial y t el final
+    grafo=cargarGrafo("esDivisorDe-200.json")
+    s, t="1", "192" #Sea s el nodo inicial y t el final
     camino = buscarPaso(grafo, s, t)
     if camino:
         print("Secuencia de nodos para recrear el paso:")
         print(" -> ".join(camino))
     else:
-        print(f"No existe camino desde {s} hasta {t}.")
+        print(f"No existe camino desde "+s+" hasta"+t)
 main()
