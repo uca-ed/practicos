25947e932f953d69ce724c0a8f8df618f9f1ebff
mercedescaranti <mercedescaranti@uca.edu.ar>
2025-10-15 11:23:01 -0300

    Ej 3, 5 TP2

--- 02.EstructurasLineales/ejercicio3.py
+++ 02.EstructurasLineales/ejercicio3.py
@@ -1,1 +1,39 @@
 """3. Representar listas por medio de celdas con enlace simple.  """
+
+# Clase que representa una celda o nodo
+class Nodo:
+    def __init__(self, dato):
+        self.dato = dato      
+        self.siguiente = None 
+
+class ListaEnlazada:
+    def __init__(self):
+        self.cabeza = None  
+
+    def agregar(self, dato):
+        nuevo = Nodo(dato)
+        if self.cabeza is None:
+            self.cabeza = nuevo
+        else:
+            actual = self.cabeza
+            while actual.siguiente:
+                actual = actual.siguiente
+            actual.siguiente = nuevo
+
+
+    def mostrar(self):
+        actual = self.cabeza
+        while actual:
+            print(actual.dato, end=" , ")
+            actual = actual.siguiente
+        print("None")
+
+
+def main():
+    lista = ListaEnlazada()
+    lista.agregar(10)
+    lista.agregar(20)
+    lista.agregar(30)
+    lista.mostrar()
+
+main()
--- 02.EstructurasLineales/ejercicio5.py
+++ 02.EstructurasLineales/ejercicio5.py
@@ -1,2 +1,64 @@
 """5. Implementar en un lenguaje de programación el algoritmo de cálculo de T-Sort basado en un grafo. 
 De no ser posible calcularlo, indicar que la estructura es cíclica.  """
+
+from collections import deque
+
+def MinUnico(G):
+    indeg = {nodo: 0 for nodo in G}
+    for origen in G:
+        for destino in G[origen]:
+            indeg[destino] += 1
+
+    sin_predecesores = [nodo for nodo in indeg if indeg[nodo] == 0]
+    
+    if not sin_predecesores:
+        return None
+    
+    return min(sin_predecesores)
+
+def T_Sort(G):
+    G = {nodo: list(ady) for nodo, ady in G.items()}
+
+    Q = deque()
+    OT = []  
+    x = MinUnico(G)
+    Q.append(x)
+
+    while Q:
+        x = Q.popleft()
+        OT.append(x)
+
+        if x in G:
+            G.pop(x)
+
+        for nodo in G:
+            if x in G[nodo]:
+                G[nodo].remove(x)
+
+        nuevo = MinUnico(G)
+        if nuevo is not None:
+            Q.append(nuevo)
+
+    if any(G.values()):
+        print("El grafo es ciclico")
+        return None
+
+    return OT
+
+
+def main():
+    grafo = {
+        'A': ['B', 'C'],
+        'B': ['D','A'],
+        'C': ['D'],
+        'D': []
+    }
+    G={}
+    resultado = T_Sort(grafo)
+    if resultado:
+        print("Orden topologico:", resultado)
+
+
+main()
+
+    
