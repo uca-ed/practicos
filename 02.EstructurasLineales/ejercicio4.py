9ad0f8160ce0eecb82d9e52e99703b64bd008897
iancapon <iancaponrusso@gmail.com>
2025-10-16 13:09:54 -0300

    radix sort listo

---
+++ 02.EstructurasLineales/ejercicio4.py
@@ -0,0 +1,69 @@
+def radixSortABC(lista):   
+    numeroMaximoDeLetras = 0
+    separadoPorLetra = []
+    for palabra in lista:
+        w = []
+        for l in palabra.upper():
+            w.append(l)
+        word = {"palabra":palabra,"letras":w,"n":len(w)}
+        separadoPorLetra.append(word)
+        if len(w) > numeroMaximoDeLetras:
+            numeroMaximoDeLetras = len(w)
+    
+    #for p in separadoPorLetra:        
+    #    print(p)
+    #print(f"numero maximo de letras: {numeroMaximoDeLetras}")
+
+    for palabra in separadoPorLetra:
+        espaciosEnBlanco = numeroMaximoDeLetras - palabra["n"]
+        for i in range(espaciosEnBlanco):
+            palabra["letras"].append(" ")
+
+    #for p in separadoPorLetra:        
+    #    print(p)
+    
+    #print("A"<"B") # True
+    #print("B">"C") # False
+    #print(" "<"A") # True
+    
+    colasDeOrdenado = []
+    for L in " ABCDEFGHIJKLMNÑOPQRSTUVWXYZ":
+        colasDeOrdenado.append({"letra":L,"cola":[]})
+    
+    for i in range(numeroMaximoDeLetras):
+        #######
+        #print(f"\n----paso: {i}----")
+        #for palabra in separadoPorLetra:
+        #    print(f"   {palabra["letras"]}---")
+        #######
+        for palabra in separadoPorLetra:          
+            letra = palabra["letras"][numeroMaximoDeLetras - i - 1]
+            #print(letra)
+            for L in colasDeOrdenado:
+                if L["letra"] == letra:
+                    L["cola"].append(palabra)            
+            
+        separadoPorLetra.clear()
+        for L in colasDeOrdenado:
+            for p in L["cola"]:
+                separadoPorLetra.append(p)
+            L["cola"].clear()
+        
+        
+    listaOrdenada = []
+    for p in separadoPorLetra:
+        listaOrdenada.append(p["palabra"])
+        
+    return listaOrdenada
+    
+    
+
+
+def main():
+    lista = ["perro", "avion", "zorro", "cabra", "abeja", "luna", "pato", "gato", "leon", "rana"]
+    ordenada = radixSortABC(lista)
+    
+    print(f"\nlista desordenada: {lista}")
+    print(f"\nradix sort:        {ordenada}")
+    print()
+main()
