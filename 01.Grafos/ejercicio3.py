abc7a15006688a20c289d5abd8cac24d0c483972
Guada Arganaraz <gualupe.arganaraz@gmail.com>
2026-08-25 18:22:58 -0300

    Update ejercicio3.py

--- 01.Grafos/ejercicio3.py
+++ 01.Grafos/ejercicio3.py
@@ -1,1 +1,3 @@
+//Implementar el algoritmo de obtención de paso de un nodo a otro de un grafo. La aplicación debe soportar leer el grafo desde un archivo de disco y la salida debe ser una secuencia con los nodos a recorrer para recrear el paso.
+
 
