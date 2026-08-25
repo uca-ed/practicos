c6040b9b116283884f905b5f5e69b296787e6052
julianpruzzo22 <julianpruzzo22@gmail.com>
2026-08-25 17:08:38 -0300

    Refactor graph processing code for clarity

--- 01.Grafos/Ej1(.json) -- Practica 1.py
+++ 01.Grafos/Ej1(.json) -- Practica 1.py
@@ -5,8 +5,6 @@ estructura = json.load(f)
 f.close()
 nodos = estructura['P']
 arcos = estructura['E']
-
-# c. Vecindad derecha de un nodo a
 a = '7'  
 vecindad_derecha_a = arcos[a]
 print("Vecindad derecha de", a, ":", vecindad_derecha_a)
@@ -18,10 +16,8 @@ for origen in arcos:
     for destino in arcos[origen]:
         vecindad_izq[destino].append(origen)
 
-# d. Vecindad izquierda de a 
 print("Vecindad izquierda de", a, ":", vecindad_izq[a])
 
-# a. Minimales del grafo
 minimales = []
 for nodo in nodos:
     otros = []
@@ -32,7 +28,6 @@ for nodo in nodos:
         minimales.append(nodo)
 print("Minimales del grafo:", minimales)
 
-# b. Maximales del grafo
 maximales = []
 for nodo in nodos:
     otros = []
